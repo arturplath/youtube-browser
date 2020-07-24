65d8c5ad9239bfda5b3439766e2347d2a7de7530
arturplath <arturplath@users.noreply.github.com>
2020-07-24 17:11:39 +0200

    Starting the project

---
+++ youtube-browser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-browser
+//
+//  Created by Artur Plath on 24/07/2020.
+//  Copyright © 2020 Artur Plath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
