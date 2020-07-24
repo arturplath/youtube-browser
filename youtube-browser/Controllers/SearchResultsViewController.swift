515fa3431e3f83e8c4440b90b47e6667223fcf38
arturplath <arturplath@users.noreply.github.com>
2020-07-24 23:49:10 +0200

    Added model, parsed data with JSON, started work with UI

---
+++ youtube-browser/Controllers/SearchResultsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SearchResultsViewController.swift
+//  youtube-browser
+//
+//  Created by Artur Plath on 24/07/2020.
+//  Copyright © 2020 Artur Plath. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+
+    var model = Model()
+    
+    var videos = [Video]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ youtube-browser/Models/Response.swift
@@ -0,0 +1,24 @@
+//
+//  Response.swift
+//  youtube-browser
+//
+//  Created by Artur Plath on 24/07/2020.
+//  Copyright © 2020 Artur Plath. All rights reserved.
+//
+
+import Foundation
+
+struct Response: Decodable {
+    
+    var items:[Video]?
+    
+    enum CodingKeys: String, CodingKey {
+        case items
+    }
+    
+    init(from decoder: Decoder) throws {
+        
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.items = try container.decode([Video].self, forKey: .items)
+    }
+}
