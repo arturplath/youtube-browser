6637870685e551221b47b9420e6549b0d64510d0
arturplath <arturplath@users.noreply.github.com>
2020-07-24 19:06:15 +0200

    Added modified .gitignore file since I do not want to public my API Key.

---
+++ youtube-browser/Helpers/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  youtube-browser
+//
+//  Created by Artur Plath on 24/07/2020.
+//  Copyright © 2020 Artur Plath. All rights reserved.
+//
+
+import Foundation
---
+++ youtube-browser/Models/Video.swift
@@ -0,0 +1,9 @@
+//
+//  Video.swift
+//  youtube-browser
+//
+//  Created by Artur Plath on 24/07/2020.
+//  Copyright © 2020 Artur Plath. All rights reserved.
+//
+
+import Foundation
