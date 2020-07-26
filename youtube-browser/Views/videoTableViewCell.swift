b20d66f5edde25f6398f1b407dfbf9d735cc891e
arturplath <arturplath@users.noreply.github.com>
2020-07-27 01:55:41 +0200

    View with results done

--- youtube-browser/Views/videoTableViewCell.swift
+++ youtube-browser/Views/videoTableViewCell.swift
@@ -10,8 +10,14 @@ import UIKit
 
 class videoTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var thumbnailImageView: UIImageView!
     
     @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
     
     var video:Video?
     
@@ -39,6 +45,26 @@ class videoTableViewCell: UITableViewCell {
         
         self.titleLabel.text = self.video?.title
         
+        let df = DateFormatter()
+        df.dateFormat = Constants.dateFormat
+        self.dateLabel.text = df.string(from: self.video!.publishedAt)
+        
+        let url = URL(string: self.video!.thumbnail)
+        
+        let session = URLSession.shared
+        
+        let dataTask = session.dataTask(with: url!) { (data, response, error) in
+            
+            if data == nil || error != nil {
+                return
+        }
+            let downloadedThumbnail = UIImage(data: data!)
+            
+            DispatchQueue.main.async {
+                self.thumbnailImageView.image = downloadedThumbnail!
+            }
+        }
+        dataTask.resume()
     }
 
 }
